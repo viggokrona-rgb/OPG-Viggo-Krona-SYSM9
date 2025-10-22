34ab7aac1d6a27c19b738043b8f237d82863c4b6
Viggo Krona <viggo.krona@gmail.com>
2025-10-22 11:06:16 +0200

    Added RecipeViewModel

--- CookMaster/Model/Recipe.cs
+++ CookMaster/Model/Recipe.cs
@@ -10,7 +10,7 @@ namespace CookMaster.Model
     {
         public string Title { get; set; }
         public string Ingredients { get; set; }
-        public  string Instructions { get; set; }
+        public string Instructions { get; set; }
 
         public string Description { get; set; }
         public string Author { get; set; }
---
+++ CookMaster/ViewModels/RecipeviewModel.cs
@@ -0,0 +1,138 @@
+﻿using CookMaster.Model;
+using CookMaster.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace CookMaster.ViewModels
+{
+    public class RecipeviewModel : INotifyPropertyChanged
+    {
+        private Recipe? _selectedRecipe;
+
+        public string Username { get; }
+
+        public bool IsAdmin { get; }
+
+        public ObservableCollection<Recipe> Recipes { get; private set; }
+
+        public Recipe? SelectedRecipe
+        {
+            get => _selectedRecipe;
+            set { _selectedRecipe = value; OnPropertyChanged(); }
+
+
+
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+        public ICommand DetailsCommand { get; }
+        public ICommand InfoCommand { get; }
+        public ICommand SignOutCommand { get; }
+        public ICommand UserCommand { get; }
+
+        public RecipeviewModel(string username)
+        {
+            Username = username;
+            IsAdmin = username == "admin"; // enkel admin-check
+
+            Recipes = RecipeService.GetRecipes(username, IsAdmin);
+
+            AddCommand = new RelayCommand(_ => AddRecipe());
+            RemoveCommand = new RelayCommand(_ => RemoveRecipe());
+            DetailsCommand = new RelayCommand(_ => ShowDetails());
+            InfoCommand = new RelayCommand(_ => ShowInfo());
+            SignOutCommand = new RelayCommand(window => SignOut(window as Window));
+            UserCommand = new RelayCommand(_ => OpenUserDetails());
+        }
+
+        private void AddRecipe()
+        {
+            var addWindow = new AddRecipeWindow(Username);
+            addWindow.Show();
+            Application.Current.Windows[0]?.Close(); // stäng RecipeListWindow
+        }
+
+        private void RemoveRecipe()
+        {
+            if (SelectedRecipe == null)
+            {
+                MessageBox.Show("Vänligen välj ett recept att ta bort.");
+                return;
+
+            }
+            RecipeService.RemoveRecipe(SelectedRecipe);
+            Recipes.Remove(SelectedRecipe);
+        }
+
+        private void ShowDetails()
+        {
+            if (SelectedRecipe == null)
+            {
+                MessageBox.Show("Vänligen välj ett recept för att se detaljer.");
+                return;
+            }
+            var details = new RecipeDetailsWindow(SelectedRecipe);
+            details.Show();
+        }
+
+        private void ShowInfo()
+        {
+            MessageBox.Show("CookMaster hjälper dig att skapa, visa och hantera recept.\n\n" +
+                "• Add Recipe: Lägg till nya recept\n" +
+                "• Details: Visa receptdetaljer\n" +
+                "• Remove: Ta bort markerat recept\n\n" +
+                "CookMaster AB © 2025",
+                "Om CookMaster",
+                 MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void SignOut(Window? window)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            window?.Close();
+        }
+
+        private void OpenUserDetails()
+        {
+            var userDetailsWindow = new UserDetailsWindow(Username);
+            userDetailsWindow.Show();
+        }
+
+        public class RelayCommand : ICommand
+        {
+            private readonly Action<object?> _execute;
+            private readonly Predicate<object?>? _canExecute;
+
+            public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+            {
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+            public void Execute(object? parameter) => _execute(parameter);
+            public event EventHandler? CanExecuteChanged;
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? propName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+
+
+
+    }
+}
