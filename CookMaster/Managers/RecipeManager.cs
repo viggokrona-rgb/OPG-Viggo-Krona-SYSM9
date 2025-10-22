915933bf2ca97fb970fbfcf8606699a56ce1751e
Viggo Krona <viggo.krona@gmail.com>
2025-10-22 10:03:43 +0200

    Added Recipe model and RecipeManager

---
+++ CookMaster/Managers/RecipeManager.cs
@@ -0,0 +1,27 @@
+﻿using CookMaster.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CookMaster.Services
+{
+    public static class RecipeService
+    {
+        private static readonly ObservableCollection<Recipe> _recipes = new();
+
+        public static ObservableCollection<Recipe> GetRecipes(string username, bool isAdmin)
+        {
+            return isAdmin
+                ? _recipes
+                : new ObservableCollection<Recipe>(_recipes.Where(r => r.Author == username));
+        }
+
+        public static void AddRecipe(Recipe recipe) => _recipes.Add(recipe);
+
+        public static void RemoveRecipe(Recipe recipe)
+        {
+            if (_recipes.Contains(recipe))
+                _recipes.Remove(recipe);
+        }
+    }
+}
+
---
+++ CookMaster/Model/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookMaster.Model
+{
+    public class Recipe
+    {
+        public string Title { get; set; }
+        public string Ingredients { get; set; }
+        public  string Instructions { get; set; }
+
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+
+
+    }
+}
